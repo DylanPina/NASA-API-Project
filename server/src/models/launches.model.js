31421dbcef1765d82e0773cdf7205e552f11e790
Dylan Pina <dillon_idgafos@yahoo.com>
2022-04-16 23:14:35 -0400

    Finished integrating MongoDB

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,8 +1,9 @@
-// const launches = require("./launches.mongo");
+const launchesDB = require("./launches.mongo");
+const planets = require("./planets.mongo");
+
+const DEFAULT_FLIGHT_NUMBER = 100;
 
 const launches = new Map();
-
-let latestFlightNumber = 100;
 
 const launch = {
 	flightNumber: 100,
@@ -15,27 +16,57 @@ const launch = {
 	success: true,
 };
 
-launches.set(launch.flightNumber, launch);
+saveLaunch(launch);
 
 function existsLaunchWithID(launchID) {
 	return launches.has(launchID);
 }
 
-function getAllLaunches() {
-	return Array.from(launches.values());
+async function getLatestFlightNumber() {
+	const latestLaunch = await launchesDB.findOne().sort("-flightNumber");
+
+	if (!latestLaunch) {
+		return DEFAULT_FLIGHT_NUMBER;
+	}
+
+	return latestLaunch.flightNumber;
 }
 
-function addNewLaunch(launch) {
-	latestFlightNumber++;
-	launches.set(
-		latestFlightNumber,
-		Object.assign(launch, {
-			success: true,
-			upcoming: true,
-			customers: ["G59"],
-			flightNumber: latestFlightNumber,
-		})
+async function getAllLaunches() {
+	return await launchesDB.find({}, { id: 0, __v: 0 });
+}
+
+async function saveLaunch(launch) {
+	const planet = await planets.findOne({
+		keplerName: launch.target,
+	});
+
+	if (!planet) {
+		throw new Error("No matching planet was found..");
+	}
+
+	await launchesDB.findOneAndUpdate(
+		{
+			flightNumber: launch.flightNumber,
+		},
+		launch,
+		{
+			upsert: true,
+		}
 	);
+}
+
+async function scheduleNewLaunch(launch) {
+	const newFlightNumber = (await getLatestFlightNumber()) + 1;
+
+	const newLaunch = Object.assign(launch, {
+		success: true,
+		upcoming: true,
+		customers: ["ZTM", "NASA"],
+		flightNumber: newFlightNumber,
+	});
+
+	await saveLaunch(newLaunch);
 }
 
 function abortLaunchByID(launchID) {
@@ -48,6 +79,6 @@ function abortLaunchByID(launchID) {
 module.exports = {
 	existsLaunchWithID,
 	getAllLaunches,
-	addNewLaunch,
+	scheduleNewLaunch,
 	abortLaunchByID,
 };
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,10 +1,10 @@
-const { getAllLaunches, addNewLaunch, existsLaunchWithID, abortLaunchByID } = require("../../models/launches.model");
+const { getAllLaunches, scheduleNewLaunch, existsLaunchWithID, abortLaunchByID } = require("../../models/launches.model");
 
-function httpGetAllLaunches(req, res) {
-	return res.status(200).json(getAllLaunches());
+async function httpGetAllLaunches(req, res) {
+	return res.status(200).json(await getAllLaunches());
 }
 
-function httpAddNewLaunch(req, res) {
+async function httpAddNewLaunch(req, res) {
 	const launch = req.body;
 
 	if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.target) {
@@ -20,13 +20,13 @@ function httpAddNewLaunch(req, res) {
 		});
 	}
 
-	addNewLaunch(launch);
+	await scheduleNewLaunch(launch);
 	return res.status(201).json(launch);
 }
 
 function httpAbortLaunch(req, res) {
 	const launchID = Number(req.params.id);
-	// If launch doesn't exist
+
 	if (!existsLaunchWithID(launchID)) {
 		return res.status(404).json({
 			error: "Launch not found",
