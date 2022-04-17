d595959b67db1a4e9046798731235c4c6072c0ee
Dylan Pina <dillon_idgafos@yahoo.com>
2022-04-16 20:00:38 -0400

    Connected to MongoDB & added schema

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,3 +1,5 @@
+// const launches = require("./launches.mongo");
+
 const launches = new Map();
 
 let latestFlightNumber = 100;
@@ -5,10 +7,10 @@ let latestFlightNumber = 100;
 const launch = {
 	flightNumber: 100,
 	mission: "Kepler Exploration X",
-	rocketName: "Explorer IS1",
+	rocket: "Explorer IS1",
 	launchDate: new Date("December 27, 2030"),
 	target: "Kepler-442 b",
-	customer: ["ZTM", "NASA"],
+	customers: ["ZTM", "NASA"],
 	upcoming: true,
 	success: true,
 };
--- server/src/server.js
+++ server/src/server.js
@@ -1,19 +1,31 @@
-const http = require('http');
+const http = require("http");
+const mongoose = require("mongoose");
 
-const app = require('./app');
+const app = require("./app");
 
-const { loadPlanetsData } = require('./models/planets.model');
+const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = "mongodb+srv://nasa-api:NewMoney420@cluster0.uc46y.mongodb.net/nasa?retryWrites=true&w=majority";
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+	console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+	console.error(err);
+});
+
 async function startServer() {
-  await loadPlanetsData();
-  
-  server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`);
-  });
+	await mongoose.connect(MONGO_URL);
+	await loadPlanetsData();
+
+	server.listen(PORT, () => {
+		console.log(`Listening on port ${PORT}...`);
+	});
 }
 
 startServer();
